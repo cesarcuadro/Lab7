25733c05d61aa9024fad9d1a7232aa4888646db9
cesarcuadro <cesarcuadro777@gmail.com>
2024-10-07 11:19:42 -0700

    set pair and triple class

--- src/Triple.java
+++ src/Triple.java
@@ -1,2 +1,23 @@
 public class Triple<A, B, C> {
+    private A first;
+    private B second;
+    private C third;
+
+    public Triple(A a, B b, C c) {
+        first = a;
+        second = b;
+        third = c;
+    }
+
+    public A getFirst() {
+        return first;
+    }
+
+    public B getSecond() {
+        return second;
+    }
+
+    public C getThird() {
+        return third;
+    }
 }
