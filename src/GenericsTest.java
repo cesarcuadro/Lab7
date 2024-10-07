0d606f2a43d4b3f74eec427dc2b57a6a1a59d415
cesarcuadro <cesarcuadro777@gmail.com>
2024-10-07 11:34:49 -0700

    instantiated triple class

--- src/GenericsTest.java
+++ src/GenericsTest.java
@@ -1,2 +1,12 @@
 public class GenericsTest {
+    public static void main(String[] args) {
+        Triple<String, Integer, Integer> tripleOne = new Triple<>("Dodgers", 5, 6);
+        Triple<String, Integer, Integer> tripleTwo = new Triple<>("Lakers", 10, 0);
+        Triple<String, Integer, Integer> tripleThree = new Triple<>("Kings", 7, 9);
+        Triple<String, Integer, Integer> tripleFour = new Triple<>("Padres", 6, 5);
+        Triple<String, Integer, Integer> tripleFive = new Triple<>("Bears", 4, 8);
+
+
+
+     }
 }
